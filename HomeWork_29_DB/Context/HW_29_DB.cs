ce2e93bfc2a5d73995e02ddad84696610c3e0c0f
Дмитрий Березиков <dm.berezikov@ya.ru>
2023-02-11 01:13:16 +0700

    123

--- HomeWork_29_DB/Context/HW_29_DB.cs
+++ HomeWork_29_DB/Context/HW_29_DB.cs
@@ -6,9 +6,10 @@ namespace HomeWork_29_DB.Context
     public class HW_29_DB : DbContext
     {
 
-        public DbSet<Deal> Deals { get; set; }
-        public DbSet<Product> Product { get; set; }
-        public DbSet<Buyer> Buyers { get; set; }
+       public DbSet<Product> Products { get; set; }
+       public  DbSet<Buyer> Buyers { get; set; }
+       public DbSet<Deal> Deals { get; set; }
+
 
         public HW_29_DB(DbContextOptions<HW_29_DB> options) : base(options)
         {
