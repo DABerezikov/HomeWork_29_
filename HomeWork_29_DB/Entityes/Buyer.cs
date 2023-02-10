a64dbe44854a34f43a53380825006f78a7abe9e8
Дмитрий Березиков <dm.berezikov@ya.ru>
2023-02-11 00:51:25 +0700

    11

--- HomeWork_29_DB/Entityes/Buyer.cs
+++ HomeWork_29_DB/Entityes/Buyer.cs
@@ -4,9 +4,9 @@ namespace HomeWork_29_DB.Entityes;
 
 public class Buyer : Person
 {
-    //public string Email { get; set; }
-    //public string? Phone { get; set; }
-    
-    //public Product Product { get; set; }
-    
+    public string Email { get; set; }
+    public string? Phone { get; set; }
+
+    public Product Product { get; set; }
+
 }
