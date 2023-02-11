5b812f59cc66af5b1f9f9493c7699f406eeee3f1
Дмитрий Березиков <dm.berezikov@ya.ru>
2023-02-11 11:47:48 +0700

    Переопределение метовов ToString

--- HomeWork_29_DB/Entityes/Buyer.cs
+++ HomeWork_29_DB/Entityes/Buyer.cs
@@ -7,6 +7,6 @@ public class Buyer : Person
     public string Email { get; set; }
     public string? Phone { get; set; }
 
-   
-
+    public override string ToString() => $"Покупатель {Surname} {Name} {Patronymic} {Phone} {Email}";
+    
 }
--- HomeWork_29_DB/Entityes/Deal.cs
+++ HomeWork_29_DB/Entityes/Deal.cs
@@ -1,4 +1,5 @@
 ﻿using HomeWork_29_DB.Entityes.Base;
+using System.Numerics;
 
 namespace HomeWork_29_DB.Entityes;
 
@@ -6,4 +7,6 @@ public class Deal : Entity
 {
     public virtual Product Products { get; set; }
     public virtual Buyer Buyer { get; set; }
+
+    public override string ToString() => $"Покупатель {Buyer.Surname} {Buyer.Name} {Buyer.Email} купил {Products.Name}";
 }
