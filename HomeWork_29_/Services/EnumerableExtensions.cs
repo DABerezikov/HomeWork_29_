05fae8e18ac6710aa633ed60dad1a51b9f730bee
Дмитрий Березиков <dm.berezikov@ya.ru>
2023-02-13 22:17:34 +0700

    Создание команды для загрузки продуктов и организация их загрузки в разметке

---
+++ HomeWork_29_/Services/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace System.Collections.ObjectModel;
+
+public static class EnumerableExtensions
+{
+    public static ObservableCollection<T> ToObservableCollection<T>(
+        this IEnumerable<T> items)
+    {
+        return new ObservableCollection<T>(items);
+    }
+
+    public static ObservableCollection<T> ToObservableCollection<T>(
+        this List<T> items)
+    {
+        return new ObservableCollection<T>(items);
+    }
+}
--- HomeWork_29_/ViewModels/ProductsViewModel.cs
+++ HomeWork_29_/ViewModels/ProductsViewModel.cs
@@ -1,18 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Input;
 using HomeWork_29_.Infrastructure.DebugServices;
 using HomeWork_29_DB.Entityes;
 using HW_29.Interfaces;
+using MathCore.WPF.Commands;
 using MathCore.WPF.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomeWork_29_.ViewModels;
 
 public class ProductsViewModel:ViewModel
 {
     private readonly IRepository<Product> _Product;
+
+    #region Products : ObservableCollection<Product> - Description
+
+    /// <summary>Description</summary>
+    private ObservableCollection<Product> _Products;
+
+    /// <summary>Description</summary>
+    public ObservableCollection<Product> Products
+    {
+        get => _Products;
+        set
+        {
+            if (Set(ref _Products, value))
+                _ProductViewSource.Source = value;
+            //_ProductViewSource.View.Refresh();
+            OnPropertyChanged(nameof(ProductView));
+
+        } 
+    }
+
+    #endregion
 
     #region ProductFilter : string - Искомое слово
 
@@ -35,9 +61,29 @@ public class ProductsViewModel:ViewModel
 
     public ICollectionView ProductView => _ProductViewSource.View;
 
-    private CollectionViewSource _ProductViewSource;
-    
-    public IEnumerable<Product> Products => _Product.Items.ToList();
+    private readonly CollectionViewSource _ProductViewSource;
+
+
+    #region Command LoadDataCommand - Команда для загрызки данных из репозитория
+
+    /// <summary> Команда для загрызки данных из репозитория </summary>
+    private ICommand _LoadDataCommand;
+
+    /// <summary> Команда для загрызки данных из репозитория </summary>
+    public ICommand LoadDataCommand => _LoadDataCommand
+        ??= new LambdaCommandAsync(OnLoadDataCommandExecuted, CanLoadDataCommandExecute);
+
+    /// <summary> Проверка возможности выполнения - Команда для загрызки данных из репозитория </summary>
+    private bool CanLoadDataCommandExecute() => true;
+
+    /// <summary> Логика выполнения - Команда для загрызки данных из репозитория </summary>
+    private async Task OnLoadDataCommandExecuted()
+    {
+        //Products = (await _Product.Items.ToArrayAsync()).ToObservableCollection();
+        Products = new ObservableCollection<Product>(await _Product.Items.ToArrayAsync());
+    }
+
+    #endregion
 
     public ProductsViewModel()
     :this(new DebugProductRepository())
@@ -48,7 +94,7 @@ public class ProductsViewModel:ViewModel
                 "Данный конструктор не предназначен для использования вне дизайнера студии");
         }
 
-        
+        _ = OnLoadDataCommandExecuted();
     }
 
     public ProductsViewModel(IRepository<Product> product)
@@ -57,7 +103,7 @@ public class ProductsViewModel:ViewModel
 
         _ProductViewSource = new CollectionViewSource
         {
-            Source = _Product.Items.ToArray(),
+            
             SortDescriptions =
             {
                 new SortDescription(nameof(Product.Name), ListSortDirection.Ascending)
