95cd47c108663a8b3c88616f2445d3b605e85975
Дмитрий Березиков <dm.berezikov@ya.ru>
2023-02-13 21:28:53 +0700

    Фильтрация данных

--- HomeWork_29_/ViewModels/ProductsViewModel.cs
+++ HomeWork_29_/ViewModels/ProductsViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Windows.Data;
 using HomeWork_29_.Infrastructure.DebugServices;
 using HomeWork_29_DB.Entityes;
 using HW_29.Interfaces;
@@ -12,9 +14,33 @@ public class ProductsViewModel:ViewModel
 {
     private readonly IRepository<Product> _Product;
 
-    public IQueryable<Product> Products => _Product.Items;
+    #region ProductFilter : string - Искомое слово
+
+    /// <summary>Искомое слово</summary>
+    private string _ProductFilter;
+
+    /// <summary>Искомое слово</summary>
+    public string ProductFilter
+    {
+        get => _ProductFilter;
+        set
+        {
+           if( Set(ref _ProductFilter, value))
+               _ProductViewSource.View.Refresh();
+
+        }
+    }
+
+    #endregion
+
+    public ICollectionView ProductView => _ProductViewSource.View;
+
+    private CollectionViewSource _ProductViewSource;
+    
+    public IEnumerable<Product> Products => _Product.Items.ToList();
 
     public ProductsViewModel()
+    :this(new DebugProductRepository())
     {
         if (!App.IsDesignTime)
         {
@@ -22,11 +48,31 @@ public class ProductsViewModel:ViewModel
                 "Данный конструктор не предназначен для использования вне дизайнера студии");
         }
 
-        _Product = new DebugProductRepository();
+        
     }
 
     public ProductsViewModel(IRepository<Product> product)
     {
         _Product = product;
+
+        _ProductViewSource = new CollectionViewSource
+        {
+            Source = _Product.Items.ToArray(),
+            SortDescriptions =
+            {
+                new SortDescription(nameof(Product.Name), ListSortDirection.Ascending)
+            }
+        };
+
+        _ProductViewSource.Filter += OnProductFilter;
+    }
+
+    private void OnProductFilter(object sender, FilterEventArgs e)
+    {
+        if (!(e.Item is Product product) || string.IsNullOrEmpty(ProductFilter)) return;
+
+        if (!product.Name.Contains(ProductFilter))
+            e.Accepted = false;
+       
     }
 }
